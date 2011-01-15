41ea50b137aad25635193eb39136c732ce4011a1
Oliver Gorwits <code@gorwits.me.uk>
2011-01-15 13:15:24 +0000

    add comments and re-use count()

--- lib/Net/Appliance/Session/ActionSet.pm
+++ lib/Net/Appliance/Session/ActionSet.pm
@@ -12,6 +12,7 @@ has 'sequence' => (
 
 sub BUILDARGS {
     my ($class, @rest) = @_;
+    # accept single hash ref or naked hash
     my $params = (ref $rest[0] eq ref {} and scalar @rest == 1 ? $rest[0] : {@rest});
 
     if (exists $params->{actions}) {
@@ -29,22 +30,22 @@ sub clone {
     });
 }
 
+# fiddly only because of auto_deref
+sub count { return scalar @{ scalar (shift)->sequence } }
+
+# store params to the set, used when send is passed via sprintf
 sub apply_params {
     my ($self, @params) = @_;
 
-    for (0 .. (scalar @{$self->sequence} - 1)) {
+    for (0 .. ($self->count - 1)) {
         $self->sequence->[$_]->params($params[$_] || []);
     }
     return $self;
 }
 
-# err...?
-sub count { return scalar @{ scalar (shift)->sequence } }
-
 # marshall the responses so as to move data from match to send
 sub marshall_responses {
     my $self = shift;
-    my @seq = $self->sequence;
 
     foreach my $i (1 .. ($self->count - 1)) {
         next unless $self->sequence->[$i]->type eq 'match';
