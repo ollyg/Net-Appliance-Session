41cda0c78da2083a2c6bfcdb205b77c3a959e25a
Oliver Gorwits <oliver@cpan.org>
2012-07-16 10:29:03 +0100

    Add links to transport man pages in documentation

--- lib/Net/Appliance/Session.pm
+++ lib/Net/Appliance/Session.pm
@@ -220,7 +220,10 @@ name of the target device in this parameter.
 
 Some of the transport backends can take their own options. For example with a
 serial line connection you might specify the port speed, etc. See the
-respective manual pages for each transport backend for further details.
+respective manual pages for each transport backend for further details
+(L<SSH|Net::CLI::Interact::Transport::SSH>,
+L<Telnet|Net::CLI::Interact::Transport::Telnet>,
+L<Serial|Net::CLI::Interact::Transport::Serial>).
 
 =item C<< add_library => $directory >>
 
